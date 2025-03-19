b466bbc9fdbe89334533d8b8f3fe298e5e55cd29
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2025-03-19 16:25:19 +0100

    added carousel to hero

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrls: ['./hero.component.scss']
 })
 export class HeroComponent {
+  currentIndex: number = 0;
+  images: string[] = [
+    '../../assets/images/bracelet_1.jpg',
+    '../../assets/images/bracelet_2.jpg',
+    '../../assets/images/bracelet_3.jpg'
+  ];
 
+  moveSlide(step: number): void {
+    const totalSlides = this.images.length;
+    this.currentIndex += step;
+
+    if (this.currentIndex < 0) {
+      this.currentIndex = totalSlides - 1;
+    }
+
+    if (this.currentIndex >= totalSlides) {
+      this.currentIndex = 0;
+    }
+  }
 }
