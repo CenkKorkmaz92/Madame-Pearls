671300cd41e9202f88f4cbfbf97d0a20f7525844
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2025-03-19 15:50:23 +0100

    added more components and background with filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from "./hero/hero.component";
 import { NavbarComponent } from './navbar/navbar.component';
+import { CollectionComponent } from "./collection/collection.component";
+import { AboutComponent } from "./about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroComponent, NavbarComponent],
+  imports: [RouterOutlet, HeroComponent, NavbarComponent, CollectionComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
