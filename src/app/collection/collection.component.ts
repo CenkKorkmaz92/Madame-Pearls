61cfb9f7b3529d9f575e055469da884a5af9f25a
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2025-03-20 15:29:06 +0100

    added images for collection

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -1,25 +1,69 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import this!
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-collection',
   standalone: true,
-  imports: [CommonModule], // Add this!
+  imports: [CommonModule],
   templateUrl: './collection.component.html',
   styleUrls: ['./collection.component.scss'],
 })
 export class CollectionComponent {
-  images = [
-    { src: '', alt: 'Gold Necklace' },
-    { src: '', alt: 'Handmade Bracelet' },
-    { src: '', alt: 'Elegant Pearl Necklace' },
-    { src: '', alt: 'Silver Earrings' },
-    { src: '', alt: 'Gold Necklace' },
-    { src: '', alt: 'Handmade Bracelet' },
-    { src: '', alt: 'Elegant Pearl Necklace' },
-    { src: '', alt: 'Silver Earrings' },
-    { src: '', alt: 'Elegant Pearl Necklace' },
-    { src: '', alt: 'Silver Earrings' },
+  isModalOpen = false;
+  selectedCategory: any = {};
+
+  categories = [
+    {
+      label: 'Gentlemen',
+      imgSrc: '../../assets/images/collection/men/men_6.webp',
+      "images": [
+        "../../assets/images/collection/men/men_1.webp",
+        "../../assets/images/collection/men/men_2.webp",
+        "../../assets/images/collection/men/men_3.webp",
+        "../../assets/images/collection/men/men_4.webp",
+        "../../assets/images/collection/men/men_5.webp",
+        "../../assets/images/collection/men/men_6.webp",
+        "../../assets/images/collection/men/men_7.webp"
+      ]
+    },
+    {
+      label: 'Ladys',
+      imgSrc: '../../assets/images/collection/women/women_3.webp',
+      images: [
+        "../../assets/images/collection/women/women_1.webp",
+        "../../assets/images/collection/women/women_2.webp",
+        "../../assets/images/collection/women/women_3.webp",
+        "../../assets/images/collection/women/women_4.webp",
+        "../../assets/images/collection/women/women_5.webp",
+        "../../assets/images/collection/women/women_6.webp",
+        "../../assets/images/collection/women/women_7.webp",
+        "../../assets/images/collection/women/women_8.webp",
+        "../../assets/images/collection/women/women_9.webp",
+        "../../assets/images/collection/women/women_10.webp",
+        "../../assets/images/collection/women/women_11.webp",
+        "../../assets/images/collection/women/women_12.webp",
+        "../../assets/images/collection/women/women_13.webp",
+        "../../assets/images/collection/women/women_14.webp",
+        "../../assets/images/collection/women/women_15.webp",
+        "../../assets/images/collection/women/women_16.webp",
+        "../../assets/images/collection/women/women_17.webp"
+      ],
+    },
+    {
+      label: 'Kids',
+      imgSrc: '../../assets/images/collection/kids/kids_1.webp',
+      images: ['assets/images/kids1.jpg', 'assets/images/kids2.jpg', 'assets/images/kids3.jpg'],
+    },
+    {
+      label: 'For Everyone',
+      imgSrc: '../../assets/images/collection/necklace/necklace_1.webp',
+      images: [
+        '../../assets/images/collection/necklace/necklace_1.webp',
+        '../../assets/images/collection/necklace/necklace_2.webp',
+        '../../assets/images/collection/necklace/necklace_3.webp',
+        '../../assets/images/collection/necklace/necklace_4.webp'
+      ],
+    },
   ];
 
   scrollToAbout(): void {
@@ -27,5 +71,14 @@ export class CollectionComponent {
     if (collectionElement) {
       collectionElement.scrollIntoView({ behavior: 'smooth' });
     }
-  } 
+  }
+
+  openModal(category: any): void {
+    this.selectedCategory = category;
+    this.isModalOpen = true;
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+  }
 }
