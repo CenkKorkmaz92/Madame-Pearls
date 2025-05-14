b25aada4df987eae30696a53598c04123fbcae81
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2025-05-14 11:12:00 +0200

    added scroll up arrow in footer, rotated it to match direction, reused function from other scroll arrows

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  scrollToHome(): void {
+    const collectionElement = document.getElementById('home');
+    if (collectionElement) {
+      collectionElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
